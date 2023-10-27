76545de77b90f010ec553278d04f06286020646f
jberlioz-fnal <91346493+jberlioz-fnal@users.noreply.github.com>
2023-10-27 11:41:12 -0500

    Crypto tool

--- bsf_parser.py
+++ bsf_parser.py
@@ -2,9 +2,9 @@ import subprocess
 import os
 import json
 
-from DataFormatTools.bsf_parser.bsf_commands import run_bdf, run_hdl
+from QuartusTools.BSF.bsf_commands import run_bdf, run_hdl
 
-run_bdf();
+run_hdl();
 
 
 
